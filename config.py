0f6a32f35da20a8459c93b6367f2c2598239f096
gxn <gxnhit@gmail.com>
2017-05-03 15:23:34 +0800

    new

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ WTF_CSRF_ENABLED = False
 
 SECRET_KEY = '!@#$%8F6F98EC3684AECA1DC44E1CB816E4A5^&*()'
 
-TOPO_FOLDER   = '/home/wangyu/Downloads/Concentrator/datalog/'
-DB_FOLDER     = '/home/wangyu/Downloads/Concentrator/datalog/'
-CLIENT_FOLDER = '/home/wangyu/Downloads/Concentrator/socket/'
-CONFIG_FOLDER = '/home/wangyu/Downloads/Concentrator/app/utils/Config'
+TOPO_FOLDER   = '/root/env/'
+DB_FOLDER     = '/root/env/'
+CLIENT_FOLDER = '/root/Concentrator/socket/'
+CONFIG_FOLDER = '/root/Concentrator/app/utils/Config'
