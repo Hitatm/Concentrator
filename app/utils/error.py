88a1e674227a46f053a17c0e23a0df464a8623a7
Atozzio <winzzhhzzhh@163.com>
2017-05-17 18:52:39 +0800

    syntime error hchart

--- app/utils/error.py
+++ app/utils/error.py
@@ -88,14 +88,14 @@ def data_error_new(time1,time2):
         warning_detail = dict()
         warning_dict_temp["value"] = 30
         warning_dict_temp["time"] = time2.encode('ascii')
-        print warning_dict_temp["time"] 
+        # print warning_dict_temp["time"] 
         warning_detail["text"] = "No Current Error"
         warning_detail["link"] = "javascript:;"
         warning_dict_temp["detail"] = warning_detail
         warning_dict_i["name"] = "None"
         warning_dict_i["evolution"] = [warning_dict_temp]
         warning_list_i.append(warning_dict_i)
-    print warning_list_i
+    # print warning_list_i
     
     vdata = DATABASE.my_db_execute('select volage, NodeID, currenttime from NetMonitor where currenttime >= ? and currenttime <= ? and volage<3;',(time1, time2))
     if vdata:
@@ -136,14 +136,14 @@ def data_error_new(time1,time2):
         warning_detail = dict()
         warning_dict_temp["value"] = 30
         warning_dict_temp["time"] = time1.encode('ascii')
-        print warning_dict_temp["time"]  
+        # print warning_dict_temp["time"]  
         warning_detail["text"] = "No Voltage Error"
         warning_detail["link"] = "javascript:;"
         warning_dict_temp["detail"] = warning_detail
         warning_dict_v["name"] = "None"
         warning_dict_v["evolution"] = [warning_dict_temp]
         warning_list_v.append(warning_dict_v)
-    print warning_list_v
+    # print warning_list_v
     nodeinfo = DATABASE.my_db_execute('select * from NodePlace;',None)
     lists = list() # nodeplace info
     for info in nodeinfo:
@@ -153,69 +153,92 @@ def data_error_new(time1,time2):
     return warning_list_v,warning_list_i,lists
 
 def syn_error(time1,time2):
-    # warning_list = list()
-    # idata = DATABASE.my_db_execute('select NodeID, currenttime, syntime from NetMonitor where currenttime >= ? and currenttime <= ? and (syntime>10 or syntime<-10) ;',(time1, time2))
-    # for data in idata:
+    # warning_list= list()    
+    # nodeset = set()
+    # data = DATABASE.my_db_execute('select syntime, NodeID, currenttime from NetMonitor where currenttime >= ? and currenttime <= ? and (syntime>30 or syntime<-30);',(time1, time2))
+    # if data:
+    #     nodedict = dict()
+    #     for i in data:
+    #         warning_dict_temp = dict()
+    #         if i[1] in nodedict.keys():
+    #             evolution_dict_temp = dict()
+    #             warning_detail = dict()
+    #             evolution_dict_temp["time"] = str(i[2]).encode('ascii')
+    #             evolution_dict_temp["value"] = 50
+    #             warning_detail["text"] = i[1].encode('ascii')+":Syntime="+str(i[0])+"ms"
+    #             warning_detail["link"] = "javascript:;"
+    #             evolution_dict_temp["detail"] = warning_detail
+    #             evolution_list = nodedict[i[1]]["evolution"]
+    #             evolution_list.append(evolution_dict_temp)
+    #             warning_dict_temp["name"] = i[1].encode('ascii')
+    #             # print warning_dict_temp["name"]
+    #             warning_dict_temp["evolution"] = evolution_list
+    #         else:
+    #             nodeset.add(i[1])
+    #             evolution_dict_temp = dict()
+    #             warning_detail = dict()
+    #             evolution_list = list()
+    #             evolution_dict_temp["time"] = str(i[2]).encode('ascii')
+    #             evolution_dict_temp["value"] = 50
+    #             warning_detail["text"] = i[1].encode('ascii')+":Syntime="+str(i[0])+"ms"
+    #             warning_detail["link"] = "javascript:;"
+    #             evolution_dict_temp["detail"] = warning_detail
+    #             evolution_list.append(evolution_dict_temp)
+    #             warning_dict_temp["name"] = i[1].encode('ascii') #NodeID
+    #             warning_dict_temp["evolution"] = evolution_list
+    #             nodedict[i[1]]=warning_dict_temp
+    #     # print nodedict.keys()
+    #         warning_list.append(warning_dict_temp)
+    #     # print warning_list
+    # else:
     #     warning_dict = dict()
-    #     warning_dict["NodeID"] = data[0]
-    #     warning_dict["warn"] = "syntime = " + str(data[2]) + "s"
-    #     warning_dict["time"] = data[1] #currenttime
+    #     warning_dict_temp = dict()
+    #     warning_detail = dict()
+    #     warning_dict_temp["value"] = 30
+    #     warning_dict_temp["time"] = time2.encode('ascii')
+    #     warning_detail["text"] = "No Syntime Error"
+    #     warning_detail["link"] = "javascript:;"
+    #     warning_dict_temp["detail"] = warning_detail
+    #     warning_dict["name"] = "None"
+    #     warning_dict["evolution"] = [warning_dict_temp]
     #     warning_list.append(warning_dict)
-    # return warning_list
-    timed=time.time()
-    warning_list= list()    
-    nodeset = set()
-    data = DATABASE.my_db_execute('select syntime, NodeID, currenttime from NetMonitor where currenttime >= ? and currenttime <= ? and (syntime>30 or syntime<-30);',(time1, time2))
-    if data:
-        nodedict = dict()
-        for i in data:
-            warning_dict_temp = dict()
-            if i[1] in nodedict.keys():
-                evolution_dict_temp = dict()
-                warning_detail = dict()
-                evolution_dict_temp["time"] = str(i[2]).encode('ascii')
-                evolution_dict_temp["value"] = 50
-                warning_detail["text"] = i[1].encode('ascii')+":Syntime="+str(i[0])+"ms"
-                warning_detail["link"] = "javascript:;"
-                evolution_dict_temp["detail"] = warning_detail
-                evolution_list = nodedict[i[1]]["evolution"]
-                evolution_list.append(evolution_dict_temp)
-                warning_dict_temp["name"] = i[1].encode('ascii')
-                # print warning_dict_temp["name"]
-                warning_dict_temp["evolution"] = evolution_list
-            else:
-                nodeset.add(i[1])
-                evolution_dict_temp = dict()
-                warning_detail = dict()
-                evolution_list = list()
-                evolution_dict_temp["time"] = str(i[2]).encode('ascii')
-                evolution_dict_temp["value"] = 50
-                warning_detail["text"] = i[1].encode('ascii')+":Syntime="+str(i[0])+"ms"
-                warning_detail["link"] = "javascript:;"
-                evolution_dict_temp["detail"] = warning_detail
-                evolution_list.append(evolution_dict_temp)
-                warning_dict_temp["name"] = i[1].encode('ascii') #NodeID
-                warning_dict_temp["evolution"] = evolution_list
-                nodedict[i[1]]=warning_dict_temp
-        # print nodedict.keys()
-            warning_list.append(warning_dict_temp)
-        # print warning_list
-    else:
-        warning_dict = dict()
-        warning_dict_temp = dict()
-        warning_detail = dict()
-        warning_dict_temp["value"] = 30
-        warning_dict_temp["time"] = time2.encode('ascii')
-        warning_detail["text"] = "No Syntime Error"
-        warning_detail["link"] = "javascript:;"
-        warning_dict_temp["detail"] = warning_detail
-        warning_dict["name"] = "None"
-        warning_dict["evolution"] = [warning_dict_temp]
-        warning_list.append(warning_dict)
+    # nodeinfo = DATABASE.my_db_execute('select * from NodePlace;',None)
+    # lists = list() # nodeplace info
+    # for info in nodeinfo:
+    #     if info[1] in nodeset:
+    #         lists.append([info[1],info[2],info[3]])
+    # # print time.time()-timed
+    # return warning_list,lists
+    data_list = list() #形如[ [Date.UTC(1970,  9, 27), 0],[Date.UTC(1970, 10, 10), 0.6 ],...]
+    nodes = set()
+    dlist=  list()
+    Data_set = DATABASE.my_db_execute(("select NodeID,syntime,currenttime from NetMonitor where currenttime >= ? and currenttime <= ? and (syntime>30 or syntime<-30);"),(time1, time2))    
+    for x in Data_set:
+        if x[0] not in nodes:
+            nodes.add(x[0])
+        dicts=dict()
+        time_ms = int(time.mktime(time.strptime(x[2],'%Y-%m-%d %H:%M:%S'))*1000)
+        dicts["name"] = x[0].encode('ascii')
+        dicts["data"] = [int(time_ms),int(x[1])]
+        dlist.append(dicts)     
+        # {'data': [1493568035000L, 835], 'name': u'0101'}
+
+    dicttemp=dict()
+    for x in dlist:
+        if x["name"] in dicttemp:
+            dicttemp[x["name"]].append(x["data"])
+        else:
+            dicttemp[x["name"]]=[x["data"]]
+
+    for key,value in dicttemp.items():
+        dicts = dict()
+        dicts["name"] = key
+        dicts["data"] = value
+        # print dicts
+        data_list.append(dicts)
     nodeinfo = DATABASE.my_db_execute('select * from NodePlace;',None)
     lists = list() # nodeplace info
     for info in nodeinfo:
-        if info[1] in nodeset:
+        if info[1] in nodes:
             lists.append([info[1],info[2],info[3]])
-    print time.time()-timed
-    return warning_list,lists
+    return data_list,lists
